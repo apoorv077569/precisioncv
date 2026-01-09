538ecc6b412309c449fb8e0cfbbd935007ef66d4
Apoorv Rathore <0211ite094@niet.co.in>
2026-01-09 11:52:57 +0530

    Modify splash screen and add assets

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen>{
   @override
   void initState(){
     super.initState();
-    Future.delayed(const Duration(seconds: 2),() async {
+    Future.delayed(const Duration(seconds: 3),() async {
       final loggedIn = await SessionService.isLoggedIn();
 
       if(loggedIn){
